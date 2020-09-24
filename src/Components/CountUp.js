34eaf7dccc4396a1e70c96cdd90987aa48d4ed79
Dpavaman <67939138+Dpavaman@users.noreply.github.com>
2020-09-25 02:01:23 +0530

    Initial Commit

---
+++ src/Components/CountUp.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react'
+
+class CountUp extends Component {
+    constructor(){
+        super()
+        this.state = {
+            count :  0
+        }
+    }
+
+    plus=()=>{
+        let count = this.state.count
+        this.setState({
+            count : count+1
+        })
+    }
+
+    minus = () =>{
+        let count = this.state.count
+        this.setState({
+            count : count - 1
+        })
+    }
+    
+
+    reset = () =>{
+        this.setState({
+            count : 0
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <h1> Counting : {this.state.count} </h1>
+                <button onClick={this.plus}>Click to Count Up</button>
+                <button onClick={this.reset}>Click to Reset</button>
+                <button onClick={this.minus}>Click to Count Down</button>
+            </div>
+        )
+    }
+}
+
+export default CountUp
